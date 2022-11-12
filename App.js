b8e5ca6736df14a4b9b2e2965fca7568f6c281c6
Michalina Igla <michalina.igla@gmail.com>
2022-11-12 20:38:39 +0100

    feat: add icons package

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your appssdddd!</Text>
+      <Text>TESSTssssffffsds</Text>
+      <Ionicons name="heart-outline" ></Ionicons>
       <StatusBar style="auto" />
     </View>
   );
