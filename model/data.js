91c99743a0ea838c6ac2476be14d82185e4c7a76
Michalina Igla <michalina.igla@gmail.com>
2022-12-26 13:47:39 +0100

    feat: music

--- model/data.js
+++ model/data.js
@@ -1,20 +1,26 @@
 export const songs = [
     {
+    id: 1,
     title:"Guitar Songs",
     artist: "Billie Eilish",
     image: require("../assets/song-images/Billie_Eilish_-_Guitar_Songs.png"),
-    id: 1
+    url: require("../assets/song-music/Billie-Eilish-guitar-songs.mp3"),
+    duration: 481,
   },
   {
+    id: 2,
     title:"Dynamite",
     artist: "BTS",
     image: require("../assets/song-images/BTS_-_Dynamite.png"),
-    id: 2
+    url: require("../assets/song-music/Billie-Eilish-guitar-songs.mp3"),
+    duration: 481,
   },
     {
+    id: 3,
     title:"3",
     artist: "3",
     image: require("../assets/song-images/BTS_-_Dynamite.png"),
-    id: 3
+    url: require("../assets/song-music/Billie-Eilish-guitar-songs.mp3"),
+    duration: 481,
   }
 ]
